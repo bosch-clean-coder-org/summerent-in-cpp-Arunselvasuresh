2080482a23b25b175aadd180ff8bc9208ee4fbd3
Arunselvasuresh <136575684+Arunselvasuresh@users.noreply.github.com>
2023-06-23 18:44:03 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,64 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+TEST_CASE("Temperature Breach Classification - Passive Cooling") {
+  BatteryCharacter batteryChar{PASSIVE_COOLING};
+
+  SECTION("Temperature within limits") {
+    double temperature = 25.0;
+    REQUIRE(classifyTemperatureBreach(batteryChar.coolingType, temperature) == NORMAL);
+  }
+
+  SECTION("Temperature too low") {
+    double temperature = -5.0;
+    REQUIRE(classifyTemperatureBreach(batteryChar.coolingType, temperature) == TOO_LOW);
+  }
+
+  SECTION("Temperature too high") {
+    double temperature = 40.0;
+    REQUIRE(classifyTemperatureBreach(batteryChar.coolingType, temperature) == TOO_HIGH);
+  }
 }
+
+TEST_CASE("Temperature Breach Classification - Hi-Active Cooling") {
+  BatteryCharacter batteryChar{HI_ACTIVE_COOLING};
+
+  SECTION("Temperature within limits") {
+    double temperature = 35.0;
+    REQUIRE(classifyTemperatureBreach(batteryChar.coolingType, temperature) == NORMAL);
+  }
+
+  SECTION("Temperature too low") {
+    double temperature = -10.0;
+    REQUIRE(classifyTemperatureBreach(batteryChar.coolingType, temperature) == TOO_LOW);
+  }
+
+  SECTION("Temperature too high") {
+    double temperature = 50.0;
+    REQUIRE(classifyTemperatureBreach(batteryChar.coolingType, temperature) == TOO_HIGH);
+  }
+}
+
+TEST_CASE("Check and Alert - Send to Controller") {
+  BatteryCharacter batteryChar{HI_ACTIVE_COOLING};
+  double temperature = 40.0;
+
+  SECTION("Breach Detected") {
+    std::ostringstream oss;
+    oss << "To: Controller\nBreach Detected\n";
+    Catch::StringStream::out() = oss.str();
+
+    checkAndAlert(TO_CONTROLLER, batteryChar, temperature);
+    REQUIRE(Catch::StringStream::out() == oss.str());
+  }
+
+  SECTION("No Breach Detected") {
+    std::ostringstream oss;
+    Catch::StringStream::out() = oss.str();
+
+    checkAndAlert(TO_CONTROLLER, batteryChar, 35.0);
+    REQUIRE(Catch::StringStream::out() == oss.str());
+  }
+}
+
+
